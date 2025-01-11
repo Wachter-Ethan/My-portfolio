10a0d3df5196bc96bb3754abc1242a8b3e4b9683
Wachter Ethan <ethan.wachter08@gmail.com>
2025-01-11 09:22:43 -0400

    sauvegard : modif titre, ajout CV et modification css et JS du button CV

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function btn_CV() {
     
     if (section.style.display === "none" || section.style.display === "") {
         section.style.display = "block";
+        document.getElementsByClassName("btn_CV")[0].innerHTML = "↑";
     } else {
         section.style.display = "none";
+        document.getElementsByClassName("btn_CV")[0].innerHTML = "Cliquez ici pour voire mon CV.";
     }
 }
