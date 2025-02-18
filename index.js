f3fa92fc02e3a6028e2f8378a5a6955186161cb7
Wachter Ethan <ethan.wachter08@gmail.com>
2025-02-18 17:56:04 -0400

    sauvegarde : fin site global.

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ function img_projet_1() {
     if (menu.classList.contains("page_menu_on")) {
         menu.classList.remove("page_menu_on");
         menu.classList.add("page_menu_off");
-        projet_1.classList.remove("projet_cine_off")
-        projet_1.classList.add("projet_cine_on")
+        projet_1.classList.remove("projet_cine_off");
+        projet_1.classList.add("projet_cine_on");
     } else {
         menu.classList.remove("page_menu_off");
         menu.classList.add("page_menu_on");
-        projet_1.classList.remove("projet_cine_on")
-        projet_1.classList.add("projet_cine_off")
+        projet_1.classList.remove("projet_cine_on");
+        projet_1.classList.add("projet_cine_off");
     }
 
     window.scrollTo({
@@ -34,18 +34,73 @@ function img_projet_1() {
 
 function img_projet_2() {
     let menu = document.getElementById("page_menu");
-    let projet_1 = document.getElementById("projet_pyxel");
+    let projet_2 = document.getElementById("projet_pyxel");
 
     if (menu.classList.contains("page_menu_on")) {
         menu.classList.remove("page_menu_on");
         menu.classList.add("page_menu_off");
-        projet_1.classList.remove("projet_pyxel_off")
-        projet_1.classList.add("projet_pyxel_on")
+        projet_2.classList.remove("projet_pyxel_off")
+        projet_2.classList.add("projet_pyxel_on")
     } else {
         menu.classList.remove("page_menu_off");
         menu.classList.add("page_menu_on");
-        projet_1.classList.remove("projet_pyxel_on")
-        projet_1.classList.add("projet_pyxel_off")
+        projet_2.classList.remove("projet_pyxel_on");
+        projet_2.classList.add("projet_pyxel_off");
+    }
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function img_projet_3() {
+    let menu = document.getElementById("page_menu");
+    let projet_3 = document.getElementById("projet_flask");
+
+    if (menu.classList.contains("page_menu_on")) {
+        menu.classList.remove("page_menu_on");
+        menu.classList.add("page_menu_off");
+        projet_3.classList.remove("projet_flask_off");
+        projet_3.classList.add("projet_flask_on");
+    } else {
+        menu.classList.remove("page_menu_off");
+        menu.classList.add("page_menu_on");
+        projet_3.classList.remove("projet_flask_on");
+        projet_3.classList.add("projet_flask_off");
+    }
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function retour_menu() {
+    let menu = document.getElementById("page_menu");
+    let projet_3 = document.getElementById("projet_flask");
+    let projet_2 = document.getElementById("projet_pyxel");
+    let projet_1 = document.getElementById("projet_cine");
+
+    if (menu.classList.contains("page_menu_off")) {
+        menu.classList.remove("page_menu_off");
+        menu.classList.add("page_menu_on");
+        projet_3.classList.remove("projet_flask_on");
+        projet_3.classList.add("projet_flask_off");
+        projet_2.classList.remove("projet_pyxel_on");
+        projet_2.classList.add("projet_pyxel_off");
+        projet_1.classList.remove("projet_cine_on");
+        projet_1.classList.add("projet_cine_off");
+
+    } else {
+        menu.classList.remove("page_menu_on");
+        menu.classList.add("page_menu_off");
+        projet_3.classList.remove("projet_flask_off");
+        projet_3.classList.add("projet_flask_on");
+        projet_2.classList.remove("projet_pyxel_off");
+        projet_2.classList.add("projet_pyxel_on");
+        projet_1.classList.remove("projet_cine_off");
+        projet_1.classList.add("projet_cine_on");
     }
 
     window.scrollTo({
